eb0b40539faba7ad22d9e66a6f61e686aef3c118
ian <ian@845bbffb-5c18-0410-91b3-f25c072b94c1>
2006-11-14 18:51:43 +0000

    inherit_views: Added Ardes::TeslyReporter integration
    git-svn-id: https://svn.ardes.com/rails_plugins/inherit_views@100 845bbffb-5c18-0410-91b3-f25c072b94c1

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,10 @@ require 'test/unit'
 require File.expand_path(File.join(File.dirname(__FILE__), '../../../../config/environment.rb'))
 require 'test_help'
 
+if defined?(Ardes::TeslyReporter)
+  Ardes::TeslyReporter.plugin_name = File.basename(File.expand_path(File.join(__FILE__, '../..')))
+end
+
 class Test::Unit::TestCase # :nodoc:
   self.use_transactional_fixtures = true
   self.use_instantiated_fixtures  = false

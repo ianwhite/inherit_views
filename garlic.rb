6be080d255a8bc5e7c02d1d62daeafcb7846dce9
Ian White <ian.w.white@gmail.com>
2008-11-20 19:15:17 +1100

    Updated CI to use new garlic gem stylee

--- garlic.rb
+++ garlic.rb
@@ -1,22 +1,6 @@
-# This is for running specs against target versions of rails
-#
-# To use do
-#   - cp garlic_example.rb garlic.rb
-#   - rake get_garlic
-#   - [optional] edit this file to point the repos at your local clones of
-#     rails, rspec, and rspec-rails
-#   - rake garlic:all
-#
-# All of the work and dependencies will be created in the galric dir, and the
-# garlic dir can safely be deleted at any point
-
 garlic do
-  # default paths are 'garlic/work', and 'garlic/repos'
-  # work_path 'garlic/work'
-  # repo_path 'garlic/repos'
+  repo 'inherit_views', :path => '.'
 
-  # repo, give a url, specify :local to use a local repo (faster
-  # and will still update from the origin url)
   repo 'rails', :url => 'git://github.com/rails/rails' #,  :local => "~/dev/vendor/rails"
   repo 'rspec', :url => 'git://github.com/dchelimsky/rspec' #, :local => "~/dev/vendor/spec"
   repo 'rspec-rails', :url => 'git://github.com/dchelimsky/rspec-rails' #, :local => "~/dev/vendor/spec"
@@ -25,14 +9,13 @@ garlic do
   repo 'ianwhite-rspec', :url => 'git://github.com/ianwhite/rspec' #, :local => "~/dev/ianwhite/spec"
   repo 'ianwhite-rspec-rails', :url => 'git://github.com/ianwhite/rspec-rails' #, :local => "~/dev/ianwhite/spec"
   
-  repo 'inherit_views', :path => '.'
 
   # for target, default repo is 'rails', default branch is 'master'
-  target 'edge', :branch => 'origin/master' do
+  target '2.2-stable', :branch => 'origin/2-2-stable' do
     prepare do
       plugin 'inherit_views', :branch => 'origin/master', :clone => true
-      plugin 'rspec'
-      plugin 'rspec-rails' do
+      plugin 'ianwhite-rspec', :as => 'rspec'
+      plugin 'ianwhite-rspec-rails', :as => 'rspec-rails' do
         sh "script/generate rspec -f"
       end
     end

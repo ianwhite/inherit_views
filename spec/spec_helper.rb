e0a5a8003768384086f24824ea4c0a66249e7252
Ian White <ian.w.white@gmail.com>
2009-02-05 18:22:40 +0000

    Simplify spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,4 @@ require File.expand_path(File.join(File.dirname(__FILE__), "../../../../config/e
 require 'spec/rails'
 
 Spec::Runner.configure do |config|
-  config.before(:each, :behaviour_type => :controller) do
-  end
 end

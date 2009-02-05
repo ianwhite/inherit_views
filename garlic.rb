af2c69b3947b47d7861cf46e9ca6753c68af983d
Ian White <ian.w.white@gmail.com>
2009-02-05 16:55:48 +0000

    Updated garlic to include the new rails 2.3 branch (which will become the master barnch when it is working)

--- garlic.rb
+++ garlic.rb
@@ -1,58 +1,32 @@
 garlic do
   repo 'inherit_views', :path => '.'
 
-  repo 'rails', :url => 'git://github.com/rails/rails' #,  :local => "~/dev/vendor/rails"
-  repo 'rspec', :url => 'git://github.com/dchelimsky/rspec' #, :local => "~/dev/vendor/spec"
-  repo 'rspec-rails', :url => 'git://github.com/dchelimsky/rspec-rails' #, :local => "~/dev/vendor/spec"
-  
-  # these are for testing rails-2.0-2.1 branch
-  repo 'ianwhite-rspec', :url => 'git://github.com/ianwhite/rspec' #, :local => "~/dev/ianwhite/spec"
-  repo 'ianwhite-rspec-rails', :url => 'git://github.com/ianwhite/rspec-rails' #, :local => "~/dev/ianwhite/spec"
-  
+  repo 'rails', :url => 'git://github.com/rails/rails'
+  repo 'rspec', :url => 'git://github.com/dchelimsky/rspec'
+  repo 'rspec-rails', :url => 'git://github.com/dchelimsky/rspec-rails'
 
-  # for target, default repo is 'rails', default branch is 'master'
-  target '2.2-stable', :branch => 'origin/2-2-stable' do
-    prepare do
-      plugin 'inherit_views', :branch => 'origin/master', :clone => true
-      plugin 'ianwhite-rspec', :as => 'rspec'
-      plugin 'ianwhite-rspec-rails', :as => 'rspec-rails' do
-        sh "script/generate rspec -f"
+  # first is rails target, last is inherit views branch
+  [ 
+    {:rails => 'master',      :inherit_views => 'rails-2.3'},
+    {:rails => '2-2-stable',  :inherit_views => 'master'},
+    {:rails => '2-1-stable',  :inherit_views => 'rails-2.0-2.1'}
+    #{:rails => '2-0-stable',  :inherit_views => 'rails-2.0-2.1'} rspec + raisl 2.0 is not playing nice at the moment
+  ].each do |target|
+
+    target target[:rails], :branch => "origin/#{target[:rails]}" do
+      prepare do
+        plugin 'inherit_views', :branch => "origin/#{target[:inherit_views]}", :clone => true
+        plugin 'rspec', :as => 'rspec'
+        plugin 'rspec-rails', :as => 'rspec-rails' do
+          sh "script/generate rspec -f"
+        end
+      end
+      run do
+        cd "vendor/plugins/inherit_views" do
+          sh "rake spec:rcov:verify"
+        end
       end
     end
-    run do
-      cd "vendor/plugins/inherit_views" do
-        sh "rake spec:rcov:verify"
-      end
-    end
-  end
-  
-  target '2.0-stable', :branch => 'origin/2-0-stable' do
-    prepare do
-      plugin 'inherit_views', :branch => 'origin/rails-2.0-2.1', :clone => true
-      plugin 'ianwhite-rspec', :as => 'rspec'
-      plugin 'ianwhite-rspec-rails', :as => 'rspec-rails' do
-        sh "script/generate rspec -f"
-      end
-    end
-    run do
-      cd "vendor/plugins/inherit_views" do
-        sh "rake spec"
-      end
-    end
-  end
-  
-  target '2.1-stable', :branch => 'origin/2-1-stable' do
-    prepare do
-      plugin 'inherit_views', :branch => 'origin/rails-2.0-2.1', :clone => true
-      plugin 'ianwhite-rspec', :as => 'rspec'
-      plugin 'ianwhite-rspec-rails', :as => 'rspec-rails' do
-        sh "script/generate rspec -f"
-      end
-    end
-    run do
-      cd "vendor/plugins/inherit_views" do
-        sh "rake spec"
-      end
-    end
+    
   end
 end
